f48d6ac7d03a689766d100114d49316435317406
Mohammadreza Abdollahpour <reza.pour1992@gmail.com>
2023-06-08 16:56:17 +0200

    feat(dependencies): manage dependencies

---
+++ buildSrc/src/main/java/dependencies/Version.kt
@@ -0,0 +1,13 @@
+package dependencies
+
+object Version {
+    const val hilt="2.44"
+    const val fragment_ktx="1.5.7"
+    const val retrofit="2.9.0"
+    const val okhttp="4.9.0"
+    const val coroutine="1.6.3"
+    const val lifecycle="2.6.1"
+    const val nav="2.5.3"
+    const val glide="4.12.0"
+    const val room="2.5.1"
+}
